1121166c58e9939fffdf7fb36bc54e1579ba035b
EugenePetrik <eugeniy.petrik@gmail.com>
2019-07-17 21:26:05 +0300

    Add feature files

---
+++ src/test/java/runners/MainRunner.java
@@ -0,0 +1,29 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+        // Features files location
+        features = "sr/test/java/features/",
+        // Steps files location
+        glue = "step_definitions",
+        monochrome = true,
+        // Run scenarios with specific tags
+        tags= {},
+        dryRun = true,
+        // Generate reports
+        plugin = {
+                "pretty",
+                "html:target/cucumber",
+                "json:target/cucumber.json"
+        }
+
+)
+
+public class MainRunner extends AbstractTestNGCucumberTests {
+}
